4167160b3b0d018a1690bd7b38c949f63de2cc8e
Sean <submit2mxh@gmail.com>
2024-10-28 11:36:05 +0800

    update install sh

--- install_docker.sh
+++ install_docker.sh
@@ -12,22 +12,18 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
+# 将当前用户加入 docker 组以便无 sudo 权限使用
+sudo usermod -aG docker $USER
 
 # 下载并安装 Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# 将当前用户加入 docker 组以便无 sudo 权限使用
-sudo groupadd docker
-
-sudo usermod -aG docker $USER
-
 # 立即生效 Docker 组更改，无需重新登录
-newgrp docker
-
-# 验证 Docker 和 Docker Compose 是否安装成功
-docker --version
-
-docker-compose --version
+newgrp docker <<EOF
+    # 验证 Docker 和 Docker Compose 是否安装成功
+    docker --version
+    docker-compose --version
+EOF
 
 echo "Docker 和 Docker Compose 安装完成。无 sudo 权限即可使用 Docker Compose。"
